ed7b3e03f17b4914a63dc23623f2e6f18236e349
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-06-28 17:19:31 +0900

    push

--- main.rb
+++ main.rb
@@ -1,9 +1,22 @@
 require 'pathname'
 require 'tmpdir'
 
+def serverspec_version
+  lock = File.read(File.expand_path("../Gemfile.lock", __FILE__))
+  lock[/serverspec \(([0-9.]+)\)/, 1]
+end
+
 def exec(cmd)
   puts "+ #{cmd}"
   system cmd
+end
+
+def commit_push(dir)
+  exec("cd #{dir} &&
+       git diff --exit-code > /dev/null ||
+       git add .
+       git commit -m 'Update Serverspec to #{serverspec_version}' &&
+       git push")
 end
 
 exec('bundle exec ruby lib/serverspec_info.rb > serverspec_info.json')
@@ -16,10 +29,14 @@ exec("git clone https://github.com/pocke/serverspec.vim #{vim_root_dir} --depth 
 exec("cp serverspec_info.json #{vim_root_dir.join 'autoload/serverspec_info.json'}")
 exec("bundle exec ruby lib/generator/vim.rb > #{vim_root_dir.join 'neosnippets/ruby.serverspec.snip'}")
 
+commit_push(vim_root_dir)
+
 
 intellij_root_dir = dir.join('intellij-serverspec-live-templates')
 exec("git clone https://github.com/pocke/intellij-serverspec-live-templates #{intellij_root_dir} --depth 1")
 exec("bundle exec ruby lib/generator/intellij.rb > #{intellij_root_dir.join 'Serverspec.xml'}")
 
+commit_push(intellij_root_dir)
 
-# exec("rm -rf #{dir}")
+
+exec("rm -rf #{dir}")
